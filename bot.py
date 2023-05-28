e743f4af64c337e06f8871f0d8d9ab4ecc86db3b
SavenkovIgor <savenkovigor.dev@gmail.com>
2023-05-29 00:25:31 +0300

    New chat bot

--- bot.py
+++ bot.py
@@ -3,17 +3,67 @@ from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
 import logging
-import os
+import os, sys, argparse, csv
 import openai
+import requests as req
 
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())
 
-class ChatEngine:
-    model = ""
-    temp = 0
+class PromptsLib:
+    csv_file: str = 'prompts.csv'
+    prompts: dict = {}
 
-    def __init__(self, model="gpt-3.5-turbo", temp=0):
+    def download_csv(self):
+        # https://github.com/f/awesome-chatgpt-prompts/blob/main/prompts.csv
+        url = 'https://raw.githubusercontent.com/f/awesome-chatgpt-prompts/main/prompts.csv'
+        r = req.get(url, allow_redirects=True)
+
+        if r.status_code == 200:
+            open(self.csv_file, 'wb').write(r.content)
+        else:
+            print('Error downloading prompts.csv')
+
+    def load_prompts(self):
+        with open(self.csv_file, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                self.prompts[row['act']] = row['prompt']
+
+        self.load_local_prompts()
+
+    def load_local_prompts(self):
+        message_versions_prompt = """
+        You are a Conversation Improvement robot.Your main goal is, \
+        to support the truth seeking conversation style.
+
+        You should take user message and rewrite it, few times according to this rules: \
+        - Rewritten message must not move topic away from the main topic. \
+        - Rewritten message terms can be replaced with more precise ones if necessary. \
+        - Your answer should be in the user's language. \
+
+        Take user message and create 3 different versions of this message: \
+
+        Your output format should be:
+        Restate version: <restate, localized version of the message>
+
+        Polite version: <polite, localized version of the message>
+
+        Validate version: <validate, localized version of the message>
+        """
+        self.prompts['Tone editor'] = message_versions_prompt
+
+    def actors(self) -> list:
+        return list(self.prompts.keys())
+
+    def prompt(self, act: str) -> str:
+        return self.prompts[act]
+
+class ChatEngine:
+    model: str
+    temp: float
+
+    def __init__(self, model: str = "gpt-3.5-turbo", temp: float = 0):
         self.model = model
         self.temp = temp
 
@@ -21,26 +71,29 @@ class Chat:
     messages: list = []
     engine: ChatEngine
 
-    def __init__(self, engine):
+    def __init__(self, engine: ChatEngine):
         self.engine = engine
 
-    def add_system_message(self, system_message):
+    def add_system_message(self, system_message: str) -> None:
         self.messages.append({"role": "system", "content": system_message})
 
-    def add_assistant_message(self, assistant_message):
+    def add_assistant_message(self, assistant_message: str) -> None:
         self.messages.append({"role": "assistant", "content": assistant_message})
 
-    def add_message(self, user_message):
+    def add_message(self, user_message: str) -> None:
         self.messages.append({"role": "user", "content": user_message})
 
-    def get_completion(self):
+    def get_completion(self) -> openai.ChatCompletion:
         return openai.ChatCompletion.create(
             model=self.engine.model,
             messages=self.messages,
             temperature=self.engine.temp,
         )
 
-    def clear_messages(self):
+    def has_messages(self) -> bool:
+        return len(self.messages) > 0
+
+    def clear_messages(self) -> None:
         self.messages = []
 
 
@@ -48,70 +101,116 @@ class Chat:
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-openai.api_key = os.getenv('OPENAI_API_KEY')
+TELEGRAM_TOKEN   = os.getenv('TELEGRAM_TOKEN')
+openai.api_key   = os.getenv('OPENAI_API_KEY')
+allow_users_list = os.getenv('ALLOW_USERS_LIST').split(',')
 
 PORT = int(os.environ.get('PORT', 5000))
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    user = update.effective_user
-    await update.message.reply_html(
-        rf"Hello, {user.mention_html()}!\nthis is a bot to help you with your conversation skills",
-        reply_markup=ForceReply(selective=True)
-    )
+chat = Chat(ChatEngine())
+prompts = PromptsLib()
+prompts.load_prompts()
 
-async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text('Available commands:\n/fix - fix your tone\n/start - start the bot\n/help - get help')
+
+def check_user(user_name: str) -> bool:
+    return user_name in allow_users_list
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if not check_user(update.effective_user.username):
+        await update.message.reply_text('Sorry, you are not allowed to use this bot')
+        return
+
+    user = update.effective_user
+    start_msg: str = f"""
+    Hello, {user.mention_html()}!
+    This is a chat bot with different roles.
+    Available commands:
+    /start - restart the conversation
+    /list - list all available roles
+    /role role_name - set your role
+    /clear - clear your role and restart the conversation
+    """
+    start_msg = start_msg.replace('    ', '')
+    await update.message.reply_html(start_msg, reply_markup = ForceReply(selective=True))
+
+async def role_list(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if not check_user(update.effective_user.username):
+        await update.message.reply_text('Sorry, you are not allowed to use this bot')
+        return
+
+    await update.message.reply_text('Available roles: \n' + '\n'.join(prompts.actors()))
+
+async def role(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if not check_user(update.effective_user.username):
+        await update.message.reply_text('Sorry, you are not allowed to use this bot')
+        return
+
+    user_message = update.message.text
+    user_message = user_message.replace('/role ', '').strip()
+
+    if user_message not in prompts.actors():
+        await update.message.reply_text('Role not found')
+        return
+
+    chat.clear_messages()
+    role_prompt = prompts.prompt(user_message)
+    chat.add_system_message(role_prompt)
+    await update.message.reply_text('Role start prompt is:' + '\n' + role_prompt)
+
+    answer = chat.get_completion().choices[0].message["content"]
+    await update.message.reply_text(answer)
+
+async def clear(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if not check_user(update.effective_user.username):
+        await update.message.reply_text('Sorry, you are not allowed to use this bot')
+        return
+
+    chat.clear_messages()
+    await update.message.reply_text('Chat cleared')
 
 async def catch_all(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text('Sorry, I did not understand that command.')
+    if not check_user(update.effective_user.username):
+        await update.message.reply_text('Sorry, you are not allowed to use this bot')
+        return
+
+    user_message: str = update.message.text
+    answer: str = ''
+
+    if chat.has_messages():
+        chat.add_message(user_message)
+        answer = chat.get_completion().choices[0].message["content"]
+    else:
+        answer = 'Please set your role first'
+
+    await update.message.reply_text(answer)
 
 def error(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
-async def fix_tone(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    message_versions_prompt = """
-    You are a Conversation Improvement robot.Your main goal is, \
-    to support the truth seeking conversation style.
-
-    You should take user message and rewrite it, few times according to this rules: \
-    - Rewritten message must not move topic away from the main topic. \
-    - Rewritten message terms can be replaced with more precise ones if necessary. \
-    - Your answer should be in the user's language. \
-
-    Take user message and create 3 different versions of this message: \
-
-    Your output format should be:
-    Restate version: <restate, localized version of the message>
-
-    Polite version: <polite, localized version of the message>
-
-    Validate version: <validate, localized version of the message>
-    """
-
-    engine = ChatEngine()
-    user_message = update.message.text
-    chat = Chat(engine)
-    chat.add_system_message(message_versions_prompt)
-    chat.add_message(user_message)
-    answer = chat.get_completion().choices[0].message["content"]
-    chat.clear_messages()
-    await update.message.reply_text(answer)
-
 
 def main() -> None:
-    application = Application.builder().token(TELEGRAM_TOKEN).build()
+    args = argparse.ArgumentParser()
+    args.add_argument('--download-prompts', action='store_true', help='Downloads prompts.csv')
 
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help))
-    application.add_handler(CommandHandler("fix", fix_tone))
+    args = args.parse_args()
 
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, catch_all))
+    if args.download_prompts:
+        Prompts.download_csv()
 
-    application.add_error_handler(error)
+    if len(sys.argv) == 1:
+        print('Starting telegram bot')
+        app = Application.builder().token(TELEGRAM_TOKEN).build()
 
-    # application.run_webhook(port=PORT, url_path=TELEGRAM_TOKEN, webhook_url='https://tg-multipurpose-bot.herokuapp.com/' + TELEGRAM_TOKEN)
-    application.run_polling()
+        app.add_handler(CommandHandler("start", start))
+        app.add_handler(CommandHandler("list", role_list))
+        app.add_handler(CommandHandler("role", role))
+        app.add_handler(CommandHandler("clear", clear))
+
+        app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, catch_all))
+
+        app.add_error_handler(error)
+
+        app.run_polling()
 
 if __name__ == '__main__':
     main()
